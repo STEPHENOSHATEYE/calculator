704d1d70b916f460944814c483f14709456eb4e9
STEPHENOSHATEYE <155460166+STEPHENOSHATEYE@users.noreply.github.com>
2024-06-13 13:07:20 +0100

    created maths function in index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function addNumbers(a,b){
+    a = parseInt(a);
+    b = parseInt(b);
+    return a + b;
+}
 
+
+alert(divideNumbers(5,'4'));
+
+function subtractNumbers(a,b){
+    a = parseInt(a);
+    b = parseInt(b);
+    return a - b;
+}
+
+function multiplyNumbers(a,b){
+    a = parseInt(a);
+    b = parseInt(b);
+    return a * b;
+}
+
+function divideNumbers(a,b){
+    a = parseInt(a);
+    b = parseInt(b);
+    return a/b;
+}
