eaf07090c38e4223926119fb7b1983aff5e0941a
STEPHENOSHATEYE <stephenolaitanoshateye@gmail.com>
2024-06-17 14:07:48 +0100

    styled frontend

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
+// const num1 = parseInt(prompt('enter a number: '));
+// const num2 = parseInt(prompt('enter a number: '));
+// const operation = prompt('enter an operation: +, -, /, - ');
+
+function operate(num1,num2,operation){
+    let result = 0;
+    switch (operation){
+        case ('+'):
+            result += addNumbers(num1,num2);
+        break;
+        
+        case ('-'):
+            result += subtractNumbers(num1,num2);
+        break;
+
+        case ('/'):
+            result += divideNumbers(num1,num2);
+        break;
+
+        case ('*'):
+            result += multiplyNumbers(num1,num2);
+        break;
+    }
+    return result;
+};
+
+
 function addNumbers(a,b){
-    a = parseInt(a);
+    a = parseInt(a);    
     b = parseInt(b);
     return a + b;
 }
-
-
-alert(divideNumbers(5,'4'));
 
 function subtractNumbers(a,b){
     a = parseInt(a);
@@ -24,3 +48,6 @@ function divideNumbers(a,b){
     b = parseInt(b);
     return a/b;
 }
+
+
+alert(`Your result is ${operate(num1,num2,operation)}`);
