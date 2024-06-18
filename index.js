fb4d3dc1456a5a4f5c69fc4fa6b960714b7a3b0a
STEPHENOSHATEYE <155460166+STEPHENOSHATEYE@users.noreply.github.com>
2024-06-18 17:34:24 +0100

    added functions to make calculator work in Index.js

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
-// const num1 = parseInt(prompt('enter a number: '));
-// const num2 = parseInt(prompt('enter a number: '));
-// const operation = prompt('enter an operation: +, -, /, - ');
+const buttons = document.querySelectorAll('button');
+const displayResult = document.querySelector('.result-display');
+const displayOperation = document.querySelector('.operation-display');
 
-const buttons = document.querySelectorAll('button')
+let firstNumber = '';
+let operator = '';
+let secondNumber = '';
+let isFilled = false;
 
 buttons.forEach((button) => {
     button.addEventListener('click',(event)=>{
-        // alert(event.target.className)
-    })
+        let keyValue = event.target.className;
+        handleKeyPress(keyValue);
+        if (keyValue === 'equals'){
+            result = operate(secondNumber,firstNumber,operator);
+            displayResult.textContent = (result);
+        }
+        displayOperation.textContent = (`${secondNumber}   ${operator}     ${firstNumber}`);
+        
+    });
 })
+
+function handleKeyPress(keyValue){
+    //check if the value of event is a number
+    if ((!isNaN(keyValue)||keyValue === '.')){
+        firstNumber += keyValue;
+    }else if(['+','-','x','/'].includes(keyValue)){
+        if (isFilled === false){
+            operator += keyValue;
+        };
+        secondNumber += firstNumber;
+        firstNumber = '';
+        isFilled = true
+    }else{
+        
+    };
+}
+
+function displayInput(value){
+
+}
+
 
 function operate(num1,num2,operation){
     let result = 0;
@@ -34,28 +65,26 @@ function operate(num1,num2,operation){
 
 
 function addNumbers(a,b){
-    a = parseInt(a);    
-    b = parseInt(b);
+    a = parseFloat(a);    
+    b = parseFloat(b);
     return a + b;
 }
 
 function subtractNumbers(a,b){
-    a = parseInt(a);
-    b = parseInt(b);
+    a = parseFloat(a);
+    b = parseFloat(b);
     return a - b;
 }
 
 function multiplyNumbers(a,b){
-    a = parseInt(a);
-    b = parseInt(b);
+    a = parseFloat(a);
+    b = parseFloat(b);
     return a * b;
 }
 
 function divideNumbers(a,b){
-    a = parseInt(a);
-    b = parseInt(b);
+    a = parseFloat(a);
+    b = parseFloat(b);
     return a/b;
 }
 
-
-alert(`Your result is ${operate(num1,num2,operation)}`);
